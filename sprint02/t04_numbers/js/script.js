5dec24ab2cdac70fe5c172ddfb71416a3ddc73d0
Максим Тарасенко <matarasenk@e1r8p18.unit.ua>
2021-05-21 17:28:01 +0300

    check t04

--- sprint02/t04_numbers/js/script.js
+++ sprint02/t04_numbers/js/script.js
@@ -3,19 +3,35 @@ let endRange = prompt("Input second number");
 let str = "";
 
 function checkDivision(beginRange, endRange) {
-    
+
     for (let i = beginRange; i <= endRange; i++) {
         if (i % 2 === 0) {
-            console.log(str += i + " is even\n");
+            str += i + " is even\n";
 
             if (i % 3 === 0)
-                console.log(str += i + " is multiple of 3\n"); 
+                str += i + " is multiple of 3\n";
 
             if (i % 10 === 0)
-                console.log(str += i + " is multiple of 10\n");
-            }
+                str += i + " is multiple of 10\n";
         }
-        console.log(str);
+    } else { if (i % 10 === 0) 
+            str += ", is a multiple of 10";
+            
+            else if (i % 3 === 0)
+            str += i + ", is a multitiple of 3";
+
+            else if (i % 2 == 0 && i % 10 == 0);
+            str += i + ",is even, is a multitiple of 10"
+
+            else 
+            str += i + "-";
+
+            str += "\n"
+        }
+        
+       
+    }
+    console.log(str);
 }
 
 checkDivision(beginRange, endRange)
