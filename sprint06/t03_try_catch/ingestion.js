3a8866284fe3eb79e9501f43d0d7065ebfc3bebc
Максим Тарасенко <matarasenk@e1r7p14.unit.ua>
2021-06-18 17:37:41 +0300

    finished

--- sprint06/t03_try_catch/ingestion.js
+++ sprint06/t03_try_catch/ingestion.js
@@ -2,4 +2,27 @@ const {EatException} = require("./eat-exception");
 
 module.exports.Ingestion = class {
     id;
+    meal_type =['breakfast', 'lunch', 'dinner'];
+    day_of_diet;
+    products = [];
+
+    constructor(type, day) {
+        this.type = type;
+        this.day_of_diet = day;
+    }
+    setProduct(product) {
+        this.products.push(product);
+    }
+    getProductInfo(productName) {
+        let result;
+        this.products.forEach(item => {
+            if(item.name == productName) {
+                result = item;
+            }
+        });
+        return result;
+    }
+    getFromFridge(productName) {
+        let product = this.getProductInfo(productName);
+        EatException(product);
 }
---
+++ sprint06/t04_what_about_forms/server.js
@@ -0,0 +1,17 @@
+console.log(document.getElementsByTagName("form"));
+function checkForm() {
+    let form = document.getElementsByTagName("form");
+    let result = null;
+    for(let item of form[0]) {
+
+        if(item.checked) {
+            console.log(item.value);
+            result = item.value;
+        }
+    }
+    if(result == 2) {
+        alert('Ответ правильный');
+    } else {
+        alert('Вы ошиблись. Подумайте получше!');
+    }
+}
